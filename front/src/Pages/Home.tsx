b4b6a15b24ce43f835a6307f2075210bcacde345
olivier chicheportiche <olivier.chicheportiche@gmail.com>
2023-01-10 12:20:05 +0100

    hello--

--- front/src/Pages/Home.tsx
+++ front/src/Pages/Home.tsx
@@ -9,14 +9,14 @@ export default function Home() {
   return (
     <div className='home'>
       <div className='parent'>
-        <div className='div1'>hello1</div>
-        <div className='div2'>hello2</div>
+        <div className='div1'></div>
+        <div className='div2'></div>
 
           <div className='div3'>
             <Sun/>
           </div>
-        <div className='div4'>hello4</div>
-        <div className='div5'>hello5</div>
+        <div className='div4'></div>
+        <div className='div5'></div>
       </div>
     </div>
   )
