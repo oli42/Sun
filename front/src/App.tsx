ae4bd543cb3f2677647fa7f2606340cfef409444
olivier chicheportiche <olivier.chicheportiche@gmail.com>
2023-01-25 15:05:41 +0100

    -/\-

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.scss'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <div >
-      
+      <Home/>
     </div>
   )
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,15 +2,13 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.scss'
 import './index.css'
-import Home from './Pages/Home'
-import Sun from './Components/Sun'
-import Result from './Components/Result'
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className='body'>
-      <Home/>
+      <App/>
     </div>
   </React.StrictMode>,
 )
